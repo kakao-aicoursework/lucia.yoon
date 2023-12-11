ec74c7f0893bfd6f68a8f821f0b556827c6e6064
Yoonheelucia <85699859+Yoonheelucia@users.noreply.github.com>
2023-12-11 20:58:59 +0900

    Add files via upload

--- AI_LLM/AI_LLM/conversationGUIfunctionCallEx.py
+++ AI_LLM/AI_LLM/conversationGUIfunctionCallEx.py
@@ -32,43 +32,7 @@ def save_playlist_as_csv(playlist_csv):
     return f'저장에 실패했습니다. \n저장에 실패한 내용은 다음과 같습니다. \n{playlist_csv}'
 
 
-def send_message(message_log, functions, gpt_model="gpt-3.5-turbo", temperature=0.1):
-    response = openai.ChatCompletion.create(
-        model=gpt_model,
-        messages=message_log,
-        temperature=temperature,
-        functions=functions,
-        function_call='auto',
-    )
-
-    response_message = response["choices"][0]["message"]
-
-    if response_message.get("function_call"):
-        available_functions = {
-            "save_playlist_as_csv": save_playlist_as_csv,
-        }
-        function_name = response_message["function_call"]["name"]
-        fuction_to_call = available_functions[function_name]
-        function_args = json.loads(response_message["function_call"]["arguments"])
-        # 사용하는 함수에 따라 사용하는 인자의 개수와 내용이 달라질 수 있으므로
-        # **function_args로 처리하기
-        function_response = fuction_to_call(**function_args)
-
-        # 함수를 실행한 결과를 GPT에게 보내 답을 받아오기 위한 부분
-        message_log.append(response_message)  # GPT의 지난 답변을 message_logs에 추가하기
-        message_log.append(
-            {
-                "role": "function",
-                "name": function_name,
-                "content": function_response,
-            }
-        )  # 함수 실행 결과도 GPT messages에 추가하기
-        response = openai.ChatCompletion.create(
-            model=gpt_model,
-            messages=message_log,
-            temperature=temperature,
-        )  # 함수 실행 결과를 GPT에 보내 새로운 답변 받아오기
-    return response.choices[0].message.content
+save_playlist_as_csv
 
 
 def main():
